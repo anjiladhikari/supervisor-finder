34f99ff1d25ca66ffee06d6cd290009462dd6b3d
anjiladhikari <anjiladlee10@gmail.com>
2026-07-31 16:20:32 +1000

    expose public models via package __init__.py
    - Import all domain models from research_finder.models
    - Define __all__ to make the public API explicit:
      AustralianState, EvidenceSource, ProjectStatus, Publication,
      RelevanceScore, ResearcherResult, ResearchProject, SearchRequest,
      SearchResponse, SourceType, VerificationStatus

---
+++ src/research_finder/__init__.py
@@ -0,0 +1,27 @@
+from research_finder.models import (
+    AustralianState,
+    EvidenceSource,
+    ProjectStatus,
+    Publication,
+    RelevanceScore,
+    ResearcherResult,
+    ResearchProject,
+    SearchRequest,
+    SearchResponse,
+    SourceType,
+    VerificationStatus,
+)
+
+__all__ = [
+    "AustralianState",
+    "EvidenceSource",
+    "ProjectStatus",
+    "Publication",
+    "RelevanceScore",
+    "ResearcherResult",
+    "ResearchProject",
+    "SearchRequest",
+    "SearchResponse",
+    "SourceType",
+    "VerificationStatus",
+]
